c0591caf6e825f8924be57067022ba892b44638f
Enchanjii <atchupchan@gmail.com>
2026-03-15 15:32:13 +0800

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -66,6 +66,7 @@
                     </div>
 
                     <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+                        
                         <!-- Project Card 1 -->
                         <div
                             class="bg-white rounded-xl overflow-hidden shadow-md hover:shadow-lg transition-shadow border border-gray-100 flex flex-col">
